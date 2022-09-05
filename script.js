ea1d8633859e32833c4a566f90fc866005ede1c9
dev-acc-practice <development-practice@proton.me>
2022-09-06 00:05:29 +0300

    added on click event listener for numbers

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //select elements
-total = document.getElementsByClassName('total');
+total = document.querySelector('span');
 zero = document.getElementById("zero");
 one = document.getElementById("one");
 two = document.getElementById("two");
@@ -18,3 +18,22 @@ division= document.getElementById("division");
 percentage = document.getElementById("percentage");
 ac= document.getElementById("AC");
 ce = document.getElementById("CE");
+
+zero.addEventListener("click",()=> total.innerText=zero.value);
+one.addEventListener("click",()=> total.innerText=one.value);
+two.addEventListener("click",()=> total.innerText=two.value);
+three.addEventListener("click",()=> total.innerText=three.value);
+four.addEventListener("click",()=> total.innerText=four.value);
+five.addEventListener("click",()=> total.innerText=five.value);
+six.addEventListener("click",()=> total.innerText=six.value);
+seven.addEventListener("click",()=> total.innerText=seven.value);
+eight.addEventListener("click",()=> total.innerText=eight.value);
+nine.addEventListener("click",()=> total.innerText=nine.value);
+
+function add(valOne, valTwo){}
+
+function subtraction(valOne, valTwo){}
+
+function multiply(valOne, valTwo){}
+
+function divide(valOne, valTwo){}
