98b55523dc4f2435d0d572c21120cf722ea6bcc8
dev-acc-practice <development-practice@proton.me>
2022-09-07 23:16:18 +0300

    changing script.js

--- script.js
+++ script.js
@@ -1,79 +1,10 @@
 //select elements
-total = document.querySelector('span');
-zero = document.getElementById("zero");
-one = document.getElementById("one");
-two = document.getElementById("two");
-three = document.getElementById("three");
-four = document.getElementById("four");
-five = document.getElementById("five");
-six = document.getElementById("six");
-seven = document.getElementById("seven");
-eight = document.getElementById("eight");
-nine = document.getElementById("nine");
-equal = document.getElementById("equal");
-addition= document.getElementById("addition");
-subtraction= document.getElementById("subtraction");
-multiplication= document.getElementById("multiplication");
-division= document.getElementById("division");
-percentage = document.getElementById("percentage");
-ac= document.getElementById("AC");
-ce = document.getElementById("CE");
-
-//set initial text and value to zero
-total.innerText =0;
-total.setAttribute("value",zero.value);
-
-const numArr = [zero, one, two, three, four, five, six, seven, eight, nine];
-
-//clearAll
-let clearAll = ac.addEventListener("click",()=>{
-    total.innerText = zero.value;
-    //reset value to zero
-    total.setAttribute('value', zero.value);
-});
-
-//delete last digit
-let clearLast = ce.addEventListener("click",(e)=> {
-    //get length
-    let len = total.innerText.length;
-    //if length is 1 and not 0 replace last digit with zero
-    if(len==1 ){
-        total.innerText = 0;
-    }
-    //delete last digit
-    else {
-        total.innerText=total.innerText.slice(0,-1);
-    }
-
-});
-
-//get value of each clicked element
-function getValue() {
-    numArr.forEach(num => {
-        num.addEventListener("click",()=>{
-            //if the displayed number is 0 than change it to the clicked number
-            if(total.innerText==0){
-                total.innerText = num.value;
-            }
-            else {
-                //if the number is not 0 the append the numbers
-                total.innerText += num.value;
-            }
-            total.setAttribute('value', total.innerText);
-        });
-    });
-
-}
-
-getValue();
-
-//operations
-const add = (valOne, valTwo) => valOne + valTwo;
-
-const subtract=(valOne, valTwo) => valOne - valTwo;
-
-const multiply = (valOne, valTwo) => valOne * valTwo;
-
-const divide= (valOne, valTwo) => valOne / valTwo;
-
-const operate = (operator, valOne, valTwo) => {}
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const total = document.querySelector('span');
+const equals = document.getElementById('equal');
+let result = '';
+let operand = '';
+let sign = '';
+let value ='';
+let displayValue ='';
