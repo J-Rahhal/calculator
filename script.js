b7b0542426da37995daa84710b045489171e7e87
dev-acc-practice <development-practice@proton.me>
2022-09-07 23:17:46 +0300

    added getNumber & getOperator function

--- script.js
+++ script.js
@@ -6,5 +6,42 @@ const equals = document.getElementById('equal');
 let result = '';
 let operand = '';
 let sign = '';
-let value ='';
-let displayValue ='';
+let value = '';
+let displayValue = '';
+
+//get clicked number
+const getNumber= () =>{
+    total.innerText = 0;
+    numbers.forEach(number => {
+        number.addEventListener('click', () =>{
+            if(total.innerText==0){
+                total.innerText = number.value;
+                result = total.innerText;
+                total.innerText = number.value;
+                operand= number.value;
+                total.setAttribute('value', total.innerText);
+            }
+            else if(displayValue !== '')  {
+                total.innerText = displayValue;
+                result = displayValue;
+                total.setAttribute('value', result);
+                operand = number.value;
+                total.innerText = operand;
+                total.setAttribute('value', total.innerText);
+            }
+
+        });
+    });
+
+};
+
+//get clicked operator
+const getOperator = () => {
+    operators.forEach(operator => {
+        operator.addEventListener('click', () => {
+                sign = operator.value;
+                console.log(sign);
+        });
+    });
+
+}
