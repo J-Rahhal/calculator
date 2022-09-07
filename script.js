4f8afa942620a56bb96c446f657ae54501edea96
dev-acc-practice <development-practice@proton.me>
2022-09-07 23:18:40 +0300

    added operation functions

--- script.js
+++ script.js
@@ -45,3 +45,29 @@ const getOperator = () => {
     });
 
 }
+
+const add = (a, b) => a+b;
+const subtract = (a, b) => a-b;
+const multiply = (a, b) => a*b;
+const divide = (a, b) => a / b;
+
+const operate =(operator, a, b) => {
+
+    switch (operator){
+        case '+':
+            value = add(a,b);
+            break;
+        case '-':
+            value = subtract(a,b);
+            break;
+        case '*':
+            value = multiply(a,b);
+            break;
+        case '/':
+            value = divide(a,b);
+            break;
+        default:
+            break;
+    }
+    return value;
+}
